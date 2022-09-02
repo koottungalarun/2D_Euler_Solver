052c9ee20c5f75060119e05a85b3d7e4dd234cb6
koottungalarun <koottungal.arun@icloud.com>
2022-09-02 15:39:22 +0530

    Update 2D_Euler_Solver.cpp

--- src/2D_Euler_Solver.cpp
+++ src/2D_Euler_Solver.cpp
@@ -30,64 +30,59 @@ int main(int argc, char ** argv )
 
 void Simulation::Run()
 {
-	Init_Global_Param();
+  Init_Global_Param();
 
-	Generate_Mesh();
+  Generate_Mesh();
 
-	Init_Flow();
+  Init_Flow();
 
-	for (current_step = 1; current_step <= max_num_of_steps; ++current_step)
-	{
-		Solve_Time_Step();
+  for (current_step = 1; current_step <= max_num_of_steps; ++current_step) {
+    Solve_Time_Step();
 
-		Compute_Boundary();
+    Compute_Boundary();
 
-		Set_Solve_Direction('x');
-		Time_Integration();
+    Set_Solve_Direction('x');
+    Time_Integration();
+		
+    Set_Solve_Direction('y');
+    Time_Integration();
 
-		Set_Solve_Direction('y');
-		Time_Integration();
+    Post_Solve();
 
-		Post_Solve();
-
-		if (Need_Stop_Iteration())
-		{
-			break;
-		}
-
-		Set_Field();
-	}
-
-	Output_Flowfield();	
+    if (Need_Stop_Iteration()) {
+      break;
+    }
+    
+    Set_Field();
+  }
+  
+  Output_Flowfield();	
 }
 
 #ifdef _OPENMP
 void InitializeOpenMP(char** argv)
 {
-	int num_threads = 1;//无命令行参数，默认为单线程
-	if (argv[1] != NULL)
-	{
-		num_threads = atoi(argv[1]);
-	}
+  int num_threads = 1;
+  if (argv[1] != NULL) {
+    num_threads = atoi(argv[1]);
+  }
 
 #ifdef WIN32	
-	omp_set_num_threads(num_threads);
+  omp_set_num_threads(num_threads);
 #else	
-	int omp_num_threads = omp_get_num_threads();//环境变量OMP_NUM_THREADS
-	if (omp_num_threads != num_threads && argv[1] != NULL)
-	{
-		cout << "同时存在环境变量和命令行参数，以命令行线程数为准！" << endl;
-		omp_set_num_threads(num_threads);
-	}
+  int omp_num_threads = omp_get_num_threads();//OMP_NUM_THREADS
+  if (omp_num_threads != num_threads && argv[1] != NULL) {
+    cout << "同时存在环境变量和命令行参数，以命令行线程数为准！" << endl;
+    omp_set_num_threads(num_threads);
+  }
 #endif
 #pragma omp parallel 
-	{
-		if (omp_get_thread_num() == 0)
-		{
-			cout << "OpenMP线程级并行已开启，线程总数为: num_of_threads =" 
-				 << omp_get_num_threads() << endl;
-		}
-		//cout << "Hello world from open mp thread " << omp_get_thread_num() << endl;
-	}
+  {
+    if (omp_get_thread_num() == 0) {
+      cout << "OpenMP线程级并行已开启，线程总数为: num_of_threads =" 
+	   << omp_get_num_threads() << endl;
+    }
+    //cout << "Hello world from open mp thread " << omp_get_thread_num() << endl;
+  }
 }
 #endif
